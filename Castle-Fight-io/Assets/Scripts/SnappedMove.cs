1108f5b76023f40d74042824334ab4fd3176b739
gkaragoz <annotto3@gmail.com>
2019-04-24 21:13:41 +0300

    Improve SnappedMove.cs script.

--- Castle-Fight-io/Assets/Scripts/SnappedMove.cs
+++ Castle-Fight-io/Assets/Scripts/SnappedMove.cs
@@ -5,17 +5,55 @@ public class SnappedMove : MonoBehaviour {
     [SerializeField]
     private GameObject _container;
     [SerializeField]
-    private GameObject _buildingPrefab;
+    private GameObject _structureMesh;
+    [SerializeField]
+    private LayerMask _groundMask;//select the layer of your ground here, or just choose Default.
 
+    [SerializeField]
     private Vector3 _truePosition;
+    [SerializeField]
     private float _gridSize = 0.5f;
+    [SerializeField]
+    private bool _isPlacing = false;
+
+    private void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (_isPlacing) {
+                _isPlacing = false;
+                return;
+            }
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, _groundMask)) {
+                if (hit.collider.tag == "Structure") {
+                    _isPlacing = true;
+                } else {
+                    _isPlacing = false;
+                }
+            } else {
+                _isPlacing = false;
+            }
+        }
+
+        if (_isPlacing) {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, _groundMask)) {
+                if (hit.collider.tag == "Ground") {
+                    _container.transform.position = new Vector3(hit.point.x, 0f, hit.point.z);
+                }
+            } else {
+                _isPlacing = false;
+            }
+        }
+    }
 
     private void LateUpdate() {
         _truePosition.x = Mathf.Floor(_container.transform.position.x / _gridSize) * _gridSize;
         _truePosition.y = Mathf.Floor(_container.transform.position.y / _gridSize) * _gridSize;
         _truePosition.z = Mathf.Floor(_container.transform.position.z / _gridSize) * _gridSize;
 
-        _buildingPrefab.transform.position = _truePosition;
+        _structureMesh.transform.position = _truePosition;
     }
 
 }
