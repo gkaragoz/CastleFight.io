f79bb8d4c4d773e1b9c742f7f4e2624ad36bb1be
gkaragoz <annotto3@gmail.com>
2019-04-24 20:48:20 +0300

    Add FollowCursor.cs and SnappedMove.cs scripts.

---
+++ Castle-Fight-io/Assets/Scripts/SnappedMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SnappedMove : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _container;
+    [SerializeField]
+    private GameObject _buildingPrefab;
+
+    private Vector3 _truePosition;
+    private float _gridSize = 0.5f;
+
+    private void LateUpdate() {
+        _truePosition.x = Mathf.Floor(_container.transform.position.x / _gridSize) * _gridSize;
+        _truePosition.y = Mathf.Floor(_container.transform.position.y / _gridSize) * _gridSize;
+        _truePosition.z = Mathf.Floor(_container.transform.position.z / _gridSize) * _gridSize;
+
+        _buildingPrefab.transform.position = _truePosition;
+    }
+
+}
