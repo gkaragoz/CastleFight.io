a67c4618d6288c43573361340df5c4ea4b17ac6d
gkaragoz <annotto3@gmail.com>
2019-04-26 23:33:39 +0300

    Create Building_SO.cs

---
+++ Castle-Fight-io/Assets/Scripts/Building_SO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Building", menuName = "Scriptable Objects/Building")]
+public class Building_SO : ScriptableObject {
+
+    #region Properties
+
+    [SerializeField]
+    private string _name = "Building";
+
+
+    [SerializeField]
+    private GameObject _prefab;
+
+    [SerializeField]
+    private Image _thumbnailImage;
+
+    #endregion
+
+}
