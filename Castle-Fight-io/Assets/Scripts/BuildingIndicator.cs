4ad6e484f3de02fd9fb5e5a58e8487e76b70b6b9
gkaragoz <annotto3@gmail.com>
2019-04-27 18:36:32 +0300

    Delete DraggableUI.cs script.

--- Castle-Fight-io/Assets/Scripts/BuildingIndicator.cs
+++ Castle-Fight-io/Assets/Scripts/BuildingIndicator.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BuildingIndicator : DraggableUI {
+public class BuildingIndicator : Menu, IDraggable, IClickable {
 
     [SerializeField]
     private Color _readyColor;
@@ -10,6 +12,38 @@ public class BuildingIndicator : DraggableUI {
     [SerializeField]
     private Image _imgInteraction;
 
+    public Action<PointerEventData> onBeginDrag;
+    public Action<PointerEventData> onDrag;
+    public Action<PointerEventData> onEndDrag;
+
+    public Action<PointerEventData> onPointerDown;
+    public Action<PointerEventData> onPointerUp;
+    public Action<PointerEventData> onPointerClick;
+
+    public void OnBeginDrag(PointerEventData data) {
+        onBeginDrag?.Invoke(data);
+    }
+
+    public void OnDrag(PointerEventData data) {
+        onDrag?.Invoke(data);
+    }
+
+    public void OnEndDrag(PointerEventData data) {
+        onEndDrag?.Invoke(data);
+    }
+
+    public void OnPointerDown(PointerEventData data) {
+        onPointerDown?.Invoke(data);
+    }
+
+    public void OnPointerUp(PointerEventData data) {
+        onPointerUp?.Invoke(data);
+    }
+
+    public void OnPointerClick(PointerEventData data) {
+        onPointerClick?.Invoke(data);
+    }
+
     public void SetColor(bool isColliding) {
         if (isColliding) {
             _imgInteraction.color = _notReadyColor;
@@ -17,5 +51,4 @@ public class BuildingIndicator : DraggableUI {
             _imgInteraction.color = _readyColor;
         }
     }
-    
 }
